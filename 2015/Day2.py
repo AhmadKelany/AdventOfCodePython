97c6afb1a2e0f05272c69f76c8b4ee1a89649712
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-14 16:24:00 +0300

    solved day 2

--- 2015/Day2.py
+++ 2015/Day2.py
@@ -1,8 +1,33 @@
+with open('2015\day2input.txt') as f:
+    lines = f.readlines()
+
+def getBox(line):
+    return list(map(int , line.split('x')))
+
+def getArea(box):
+    return 2 * box[0] * box[1] + 2 * box[1] * box[2] + 2 * box[2] * box[0]
+
+def getSlack(box):
+    return min(box[0]*box[1] , box[0] * box[2] , box[1] * box[2])
+
+def getRibbonArea(box):
+    box.sort()
+    l = box[:2]
+    return 2 * l[0] + 2 * l[1] + box[0] * box[1] * box[2]
+
 def Part1():
-    return 5
+    sum = 0
+    for line in lines:
+        box = getBox(line)
+        sum += getArea(box) + getSlack(box)
+    return sum
 
 def Part2():
-    return 6
+    sum = 0
+    for line in lines:
+        box = getBox(line)
+        sum += getRibbonArea(box) 
+    return sum
 
 print('Part 1 :' , Part1())
 print('Part 2 :' , Part2())
