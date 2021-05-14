691c63c2b4ee816236e90876dce9cdc19225a519
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-15 02:41:19 +0300

    added some code to day 3

--- 2015/Day3.py
+++ 2015/Day3.py
@@ -2,6 +2,13 @@ with open('2015\day2input.txt') as f:
     input = f.read()
 
 def GetSeenCount(directions, moversCount):
-    return 0 
+    currentLocations = {}
+    seen = set()
+    for m in range(0,moversCount):
+        currentLocations[m] = '0,0'
 
-
+    seen.add((0,0))
+    moverIndex = 0
+    for c in directions:
+        newlocation = currentLocations[moverIndex].split(',')
+        
