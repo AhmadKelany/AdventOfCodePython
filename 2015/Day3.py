5f518ee2646c6acddf16368ca4d5db1d278bbbf9
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-15 14:53:13 +0300

    solved day 3

--- 2015/Day3.py
+++ 2015/Day3.py
@@ -1,4 +1,4 @@
-with open('2015\day2input.txt') as f:
+with open('2015\day3input.txt') as f:
     input = f.read()
 
 def GetSeenCount(directions, moversCount):
@@ -7,8 +7,30 @@ def GetSeenCount(directions, moversCount):
     for m in range(0,moversCount):
         currentLocations[m] = '0,0'
 
-    seen.add((0,0))
+    seen.add('0,0')
     moverIndex = 0
     for c in directions:
-        newlocation = currentLocations[moverIndex].split(',')
-        
+        newlocation = list(map(int , currentLocations[moverIndex].split(','))) 
+        match c:
+            case '^':
+                newlocation[1] -= 1
+            case 'v':
+                newlocation[1] += 1
+            case '>':
+                newlocation[0] += 1
+            case '<':
+                newlocation[0] -= 1
+        newlocationString = f'{newlocation[0]},{newlocation[1]}'
+        seen.add(newlocationString)
+        currentLocations[moverIndex] = newlocationString
+        moverIndex = 0 if moverIndex == moversCount -1 else moverIndex + 1
+    return len(seen)
+
+def part1():
+    return GetSeenCount(input,1)
+
+def part2():
+    return GetSeenCount(input , 2)
+
+print(f'part 1 result = {part1()}')
+print(f'part 2 result = {part2()}')
